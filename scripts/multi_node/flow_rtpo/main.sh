6fb5d48a81dea708e3e8c98f4e86bee34f3df712
xutaohenrymao <xutao.henry.mao@gmail.com>
2025-09-17 19:46:49 -0500

    update for log

--- scripts/multi_node/flow_rtpo/main.sh
+++ scripts/multi_node/flow_rtpo/main.sh
@@ -5,12 +5,10 @@ export NCCL_IB_DISABLE=0
 export NCCL_IB_HCA=mlx5
 export NCCL_IB_GID_INDEX=3
 export RANK=0
-export MASTER_ADDR=10.82.139.22
-export MASTER_PORT=19001
 
 # Launch command (parameters automatically read from accelerate_multi_node.yaml)
 accelerate launch --config_file ${PYTHONPATH}/scripts/accelerate_configs/flow_rtpo_multi_node.yaml \
     --num_machines 6 --num_processes 48 \
-    --machine_rank ${RANK} --main_process_ip ${MASTER_ADDR} --main_process_port ${MASTER_PORT} \
+    --machine_rank ${RANK} \
     ${PYTHONPATH}/scripts/train_flow_rtpo.py \
     --config_file ${PYTHONPATH}/config/flow_rtpo.py:flow_rtpo_large 
