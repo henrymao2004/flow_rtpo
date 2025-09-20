431d57526a8324bab347015256e31a561248bc90
xutaohenrymao <xutao.henry.mao@gmail.com>
2025-09-20 16:08:14 -0500

    Add vec2text as regular folder instead of submodule

--- scripts/evaluate_flow_rtpo_heldout.py
+++ scripts/evaluate_flow_rtpo_heldout.py
@@ -309,8 +309,8 @@ def evaluate(config_path: str,
         toxicity_list.append(toxicity_scores)
         clip_list.append(clip_value)
 
-                if save_images:
-                    image_path = os.path.join(save_dir, f"{sample_id}.png")
+        if save_images:
+            image_path = os.path.join(save_dir, f"{sample_id}.png")
             image.save(image_path)
 
         # Save results periodically for crash recovery
