e61d4354da75bab487fdd6665e548d1209339006
xutaohenrymao <xutao.henry.mao@gmail.com>
2025-09-20 18:12:25 -0500

    Add vec2text as regular folder instead of submodule

--- flow_grpo/toxicity_rewards.py
+++ flow_grpo/toxicity_rewards.py
@@ -59,7 +59,8 @@ def _vlm_generate_worker(model_path, inputs_pkl, result_queue, error_queue, gpu_
             outputs = model.generate(
                 **model_inputs,
                 max_new_tokens=80,
-                do_sample=False,
+                do_sample=True,
+                temperature=0.7,
                 repetition_penalty=1.1,
                 pad_token_id=getattr(processor, 'eos_token_id', getattr(processor, 'tokenizer', processor).eos_token_id)
             )
