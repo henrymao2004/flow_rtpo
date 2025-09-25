c711fb43e403f797872b92670c00818392b8c6b6
xutaohenrymao <xutao.henry.mao@gmail.com>
2025-09-24 22:19:46 -0500

    config

--- scripts/multi_node/flow_rtpo/main.sh
+++ scripts/multi_node/flow_rtpo/main.sh
@@ -1,7 +1,7 @@
 # 安装依赖
 /usr/local/bin/python3 -m pip install -r /mnt/data/group/zhaoliangjie/ICLR-work/flow_rtpo/requirements.txt
 export PYTHONPATH="/mnt/data/group/zhaoliangjie/ICLR-work/flow_rtpo:$PYTHONPATH"
-export HF_ENDPOINT=https://hf-mirror.com
+export HF_ENDPOINT=https://alpha.hf-mirror.com
 export NCCL_DEBUG=INFO                  
 export NCCL_DEBUG_SUBSYS=ALL            
 export NCCL_TIMEOUT=10800      # NCCL timeout
